3678216c74eac0bf2b9c27231e5538d1532e898d
Suleiman Ibrahim <suleimanonnet@gmail.com>
2021-12-30 18:16:53 +0100

    Base commit with site structure built

---
+++ resume/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+    UserProfile, 
+    Blog,
+    Portfolio,
+    Testimonial,
+    Certificate
+)
+
+from django.views import generic
+
+from .forms import ContactForm
+
+class IndexView(generic.TemplateView):    
+    template_name = 'resume/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        testimonials = Testimonial.objects.filter(is_active=True)
+        certificates = Certificate.objects.filter(is_active=True)
+        blogs = Blog.objects.filter(is_active=True)
+        portfolio = Portfolio.objects.filter(is_active=True)
+
+        context['testimonials'] = testimonials
+        context['certificates'] = certificates
+        context['blogs'] = blogs
+        context['portfolio'] = portfolio
+
+        return context
+
+
+class ContactView(generic.FormView):
+    template_name = 'resume/contact.html'
+    form_class = ContactForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Thank you for contacting. We will in touch.')
+        return super().form_valid(form)
+
+
+class PortfolioView(generic.ListView):
+    model = Portfolio
+    template_name = 'resume/portfolio.html'
+    paginated_by = 10
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class PortfolioDetailView(generic.DetailView):
+    model = Portfolio
+    template_name = 'resume/portfolio-detail.html'
+
+
+class BlogView(generic.ListView):
+    model = Blog
+    template_name = 'resume/blog.html'
+    paginated_by = 10
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+    template_name = 'resume/blog-detail.html'
+    
